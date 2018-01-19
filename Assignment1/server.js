c60b054b6b1b68c0df94c31bd0d81dba12b291eb
Elliot Maude <maude.elliot@gmail.com>
2018-01-19 16:44:16 -0500

    commit 2

--- Assignment1/server.js
+++ Assignment1/server.js
@@ -1,9 +1,21 @@
+/*********************************************************************************
+* WEB322: Assignment 1
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+*
+* Name: Elliot Maude      Student ID: 032830127    Date: 1/19/2018
+*
+* Online (Heroku) URL: https://morning-hollows-99559.herokuapp.com/
+*
+********************************************************************************/ 
+
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
 
 app.get("/", (req, res)=> {
-    res.send("Hello World");
+    res.send("Elliot Maude - 032830127");
 });
 
 app.listen(HTTP_PORT);
