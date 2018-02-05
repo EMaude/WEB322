a47c3ee6c7b6a2f784e7acf59affaf0b5ff22392
Elliot Maude <maude.elliot@gmail.com>
2018-02-05 13:06:21 -0800

    update 02/04/18

--- Web322-app/server.js
+++ Web322-app/server.js
@@ -1,8 +1,23 @@
+const http = require('http'); 
 const express = require('express');
-const app = express();
+const url = require('url');
+const path = require('path')
 
+const app = express();
 const port = process.env.PORT || 8080; 
+
+app.use(express.static('public'));
 
 app.listen(port, () => {
     console.log("Express http server listening on " + port);
+});
+
+app.get('/', function(req, res)
+{
+    res.sendfile("views/home.html");
+});
+
+app.get('/about', function(req, res)
+{
+    res.sendfile("views/about.html");
 });
